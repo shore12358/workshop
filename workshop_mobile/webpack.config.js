a51ff5f7efea6bb201a9aeb283eec599cfe091a2
Joan Wu <wuqiong@tuhu.cn>
2018-02-26 18:40:20 +0800

    update configuration

--- workshop_mobile/webpack.config.js
+++ workshop_mobile/webpack.config.js
@@ -10,7 +10,7 @@ const ENTRY_PATH = path.join(__dirname, './src/main.js');
 const config = {
     entry: {
         app: ENTRY_PATH,
-        vendor: ['vue-router', 'vue-multiselect', 'isomorphic-fetch', 'socket.io-client']
+        vendor: ['vue-router', 'vue-multiselect', 'socket.io-client']
     },
     output: {
         publicPath: '/WorkShopH5/',
