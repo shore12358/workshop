13ee91a7d45973b7891ea90f617c04e09d7c740b
Joan Wu <wuqiong@tuhu.cn>
2018-03-05 15:08:41 +0800

    remove things

--- workshop_mobile/src/main.js
+++ workshop_mobile/src/main.js
@@ -4,7 +4,6 @@ import App from './App';
 import router from './router';
 import infiniteScroll from 'vue-infinite-scroll';
 import store from './store/index';
-import 'es6-symbol/implement';
 import Multiselect from 'vue-multiselect';
 import TransDate from './directives/date';
 import Popout from './components/Popout';
