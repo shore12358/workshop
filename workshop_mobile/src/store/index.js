186e13b626dc56deaad55977c061aaa57c7c4dc9
shore12358 <shore12358@126.com>
2018-03-07 15:26:12 +0800

    prepared for work env

--- workshop_mobile/src/store/index.js
+++ workshop_mobile/src/store/index.js
@@ -151,6 +151,7 @@ export default new Vuex.Store({
         initAsync ({ commit }) {
             R.getAllOrders()
                 .catch(err => {
+                    alert('function getOrders is running into catch block');
                     commit({
                         type: 'init',
                         orders: [],
@@ -160,6 +161,7 @@ export default new Vuex.Store({
                     Bu.st.setKey('orderCounts', null);
                 })
                 .then(res => {
+                    alert('function getOrders is running into then block');
                     const { workshopRos, roStats, currentTime } = res.data;
                     const time_gap = currentTime - Date.now();
                     commit({
@@ -176,6 +178,7 @@ export default new Vuex.Store({
 	    fetchLineListAsync ({ commit }) {
             R.getLineList()
                 .catch(err => {
+                    alert('function getLineList is running into catch block');
                     commit({
                         type: 'fetchLineList',
                         lineList: []
@@ -183,6 +186,7 @@ export default new Vuex.Store({
                     Bu.st.setKey('lineList', null);
                 })
                 .then(res => {
+                    alert('function getLineList is running into then block');
                     const { data } = res;
                     if (data instanceof Array && data.length > 0 ) {
                         data.forEach(line => {
